de5870808daa498a65af92d59d608de3bd639372
wApMorty <wapmorty@skiff.com>
2024-07-30 17:42:50 +0200

    Closing parser in main

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import parser
 
 db = db.Database("db.db")
 db.connect()
+# parser = parser.Parser()
 
 print(db.get_champion_id("rumble"))
 
-# parser = parser.Parser()
 # winrate, games = parser.get_matchup_data("hecarim", "aatrox")
 # print(winrate)
 # print(games)
 
-#db.init_champion_table()
+# db.init_champion_table()
 
+# parser.close()
 db.close()
